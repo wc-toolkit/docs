95b6262f53c0bd2ee2a32ed486527b2b985ac903
Burton Smith <burton@breakstuff.io>
2025-01-25 13:55:41 -0500

    prep for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://wc-workbench.com',
 	integrations: [
 		starlight({
 			title: 'Web Components Workbench',
