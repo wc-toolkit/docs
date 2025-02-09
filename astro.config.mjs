310c43da5135d54e5f1dd39c415003cb1d0a6377
Burton Smith <burton@breakstuff.io>
2025-02-09 13:22:12 -0500

    update nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,17 +32,17 @@ export default defineConfig({
 						{ label: 'Custom CEM Types', slug: 'cem-utilities/types' },
 					],
 				},
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+				// {
+				// 	label: 'Guides',
+				// 	items: [
+				// 		// Each item here is one entry in the navigation menu.
+				// 		{ label: 'Example Guide', slug: 'guides/example' },
+				// 	],
+				// },
+				// {
+				// 	label: 'Reference',
+				// 	autogenerate: { directory: 'reference' },
+				// },
 			],
 			customCss: ['./src/styles/custom-styles.css'],
 		}),
